4b584cf31327509ae77cd26b1162f9ef55beb3c7
ahmadfaraj4000-gif <ahmad.faraj4000@gmail.com>
2026-05-24 18:13:10 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/rentmect-admin-portal/',
+  base: '/rentmect-client-portal/',
   server: {
     port: 5173,
     host: true,
